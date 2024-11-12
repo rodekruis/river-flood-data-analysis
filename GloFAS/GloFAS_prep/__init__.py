74d9c28301f7ab68b79008c19375775e77a27742
ERKuipers <elskuipers@protonmail.com>
2024-11-12 16:14:42 +0100

    trying to reorganize though never any module known to python

---
+++ GloFAS/GloFAS_prep/__init__.py
@@ -0,0 +1,10 @@
+# import sys 
+# import os 
+# from pathlib import Path
+
+# cur = Path(os.getcwd())
+# parent_dir = cur.parent
+# working_dir = 'c:\\Users\\els-2\\MaliGloFAS\\river_flood_data_analysis\\GloFAS_prep'
+# os.chdir(working_dir)
+# sys.path.append(working_dir)
+# print (sys.path)
--- GloFAS_data_extractor/__init__.py
+++
@@ -1,6 +0,0 @@
-
-# @valentijn7 , do i initialize now every function in this script? 
-# because what if then we import the module in another script but you only need 1 function?... 
-# and what is proper syntax? 
-
-# maybe you add your data collection here? 
