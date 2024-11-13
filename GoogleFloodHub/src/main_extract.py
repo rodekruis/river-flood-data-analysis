416b6a7d31750956f6bcd09d16467f3ba4d95d17
valentijn7 <v.w.oldenburg@student.rug.nl>
2024-11-13 22:29:11 +0100

    Small improvements to the FloodHub data extractor

--- GoogleFloodHub/src/main_extract.py
+++ GoogleFloodHub/src/main_extract.py
@@ -8,7 +8,9 @@
 # Example usage: python3 main.py Mali 01-10-2024 07-10-2024
 #
 # For a more complete instruction, see the README of:
-# https://github.com/valentijn7/GoogleFloodHub-data-extractor
+# https://github.com/valentijn7/GoogleFloodHub-data-extractor;
+# For the most recent version of the code, see:
+# https://github.com/rodekruis/river-flood-data-analysis
 
 
 import sys
@@ -20,11 +22,10 @@ def main():
     
     try:
         country, a, b = extract.validate_args(sys.argv)
-        _, _, _ = extract.extract_country_data_for_time_delta("../key.txt", country, (a, b))
-        print(
-            f"Extraction succesful for {country} with issue date {str(a)[:10]} "
-            f"and {str(b - a)[:2]} days of data"
-        )
+        _, _, forecasts = extract.extract_country_data_for_time_delta("../key.txt",
+                                                                      country,
+                                                                      (a, b))
+        extract.validate_forecasts(forecasts, (a, b), country)
     except Exception as exc:
         extract.handle_exception(exc)
 
